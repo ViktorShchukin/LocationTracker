f9893d8391061cf608a309e013962dd7c03294a0
anorisno <shukin.viktor22@gmail.com>
2024-11-16 05:28:41 +0300

    first try

--- app/src/main/java/com/anorisno/tracker/httpclient/mapper/AlarmMapper.kt
+++ app/src/main/java/com/anorisno/tracker/httpclient/mapper/AlarmMapper.kt
@@ -9,11 +9,17 @@ import org.tensorflow.lite.task.gms.vision.detector.Detection
 @Mapper(config = AppMapperConfig::class)
 interface AlarmMapper {
 
+    // this scale was got from python script. see appDataTool/pythonScripts/pixelScale/pixel_size_rad.py
+    // todo reformat it as normal constant
+    companion object pixelSize{
+        val value = 0.0018262619153803297
+    }
+
     @Mapping(target = "id", source = "cameraId")
-    @Mapping(target = "azimuth", expression = "java(detection.getBoundingBox().centerX())")
-    @Mapping(target = "elevation", expression = "java(detection.getBoundingBox().centerY())")
-    @Mapping(target = "width", expression = "java(detection.getBoundingBox().width())")
-    @Mapping(target = "height", expression = "java(detection.getBoundingBox().height())")
+    @Mapping(target = "azimuth", expression = "java(detection.getBoundingBox().centerX() * 0.0018262619153803297)")
+    @Mapping(target = "elevation", expression = "java(detection.getBoundingBox().centerY() * 0.0018262619153803297)")
+    @Mapping(target = "width", expression = "java(detection.getBoundingBox().width() * 0.0018262619153803297)")
+    @Mapping(target = "height", expression = "java(detection.getBoundingBox().height() * 0.0018262619153803297)")
     @Mapping(target = "type", expression = "java(detection.getCategories().get(0).getLabel())")
     fun toDto(cameraId: String, detection: Detection): AlarmNonRangeDto
 }
